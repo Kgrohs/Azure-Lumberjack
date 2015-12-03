15443a34cf5d1fbf1dd4227da8b074fa2c5a77b9
Konrad Grohs <konrad.grohs@alertsense.com>
2015-12-02 23:33:05 -0700

    Created a method to filter the email logs based on the form data, the email logs can then be merged in with the rest of the database logs and returned to the view.

---
+++ src/Alertsense.Azure.Lumberjack/Alertsense.Azure.Lumberjack/Helpers/LogViewerHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Alertsense.Azure.Lumberjack.Models;
+using AlertSense.Azure.Lumberjack.Contracts.Entities;
+
+namespace Alertsense.Azure.Lumberjack.Helpers
+{
+    public static class LogViewerHelper
+    {
+        public static List<SourcedAdoNetLog> FilterLogs(this List<SourcedAdoNetLog> unfilteredLogs , LogViewerParamModel filterModel )
+        {
+            var filteredLogs = filterModel.StartDate.HasValue ? unfilteredLogs.Where(x => x.Date >= filterModel.StartDate) : unfilteredLogs;
+            filteredLogs = filterModel.EndDate.HasValue ? unfilteredLogs.Where(x => x.Date <= filterModel.EndDate) : filteredLogs; // EndDate.AddSeconds(1) ??
+            filteredLogs = filterModel.LogLevel != null ? unfilteredLogs.Where(x => x.Level.ToLowerInvariant() == filterModel.LogLevel.ToLowerInvariant()) : filteredLogs; 
+            filteredLogs = filterModel.Thread != null ? unfilteredLogs.Where(x => x.Thread.ToLowerInvariant() == filterModel.Thread.ToLowerInvariant()) : filteredLogs; 
+            filteredLogs = filterModel.LoggerType != null ? unfilteredLogs.Where(x => x.Logger.ToLowerInvariant() == filterModel.LoggerType.ToLowerInvariant()) : filteredLogs; 
+
+            return filteredLogs.ToList();
+        }
+    }
+}
+
+
+
+
+//TODO *********************** Done? test it *************************************
